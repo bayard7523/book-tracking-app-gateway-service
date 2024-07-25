3023c703f224fe050c19ed152442af4bb45f9e04
bayard7523 <zheniayakymchuck@gmail.com>
2024-07-25 12:03:40 +0300

    initial commit

---
+++ src/test/java/com/bayard7523/booktrackingapp/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bayard7523.booktrackingapp.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
